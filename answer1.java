b797963c3765ae759b5c4e39f6fb58ab40cd15f3
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-10 18:25:51 +0300

    1005

---
+++ answer1.java
@@ -0,0 +1,10 @@
+public class answer1 {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a <= 20)
+        {
+            System.out.println("Считаю " +a);
+            a++;
+        }
+    }
+}
---
+++ answer3.java
@@ -0,0 +1,9 @@
+public class answer3 {
+    public static void main(String[] args) {
+        for (int a = 2; a < 12; a++) {
+            for (int b = 1; b < 11; b++) {
+                System.out.println(a + " x " + b + " = " + a * b);
+            }
+        }
+    }
+}
